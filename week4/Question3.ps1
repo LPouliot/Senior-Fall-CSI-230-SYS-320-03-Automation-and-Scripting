975b200238cc533105cb5bfb34c1f71f9ca2f7a7
Lily <LPouliot@champlain.edu>
2025-09-18 04:37:36 -0400

    Adding Question3

---
+++ week4/Question3.ps1
@@ -0,0 +1,29 @@
+﻿# Get login and logoff records from Widndows Events and save to a variable
+# Get the last 14 days 
+
+$loginouts = Get-EventLog system -Source Microsoft-Windows-winlogon -After (Get-Date).AddDays(-14)
+
+$loginoutsTable = @() #Empty Array to fill customly
+for($i=0; $i -lt $loginouts.Count; $i++){
+
+# creating event property value 
+$event = ""
+if($loginouts[$i].InstanceId -eq "7001") {$event="Logon"}
+if($loginouts[$i].InstanceId -eq "7002") {$event="Logoff"}
+
+# Creating user Property valuTe 
+# $user = $loginouts[$i].ReplacementStrings[1]
+$user = New-Object System.Security.Principal.SecurityIdentifier($loginouts[$i].ReplacementStrings[1])
+$account = $user.Translate([System.Security.Principal.NTAccount])
+$username = $account.Value
+
+
+# Adding each new line (in form of a custom object) to our empty array
+$loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated; `
+                                       "Id" = $loginouts[$i].InstanceId; `
+                                    "Event" = $event; `
+                                     "User" = $username; 
+                                     }
+} #End of for loop
+
+$loginoutsTable
