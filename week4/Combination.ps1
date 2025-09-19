aa8cb0fe9552daf1c48b8b68739c72a2b18c1141
Lily <LPouliot@champlain.edu>
2025-09-19 16:31:43 -0400

    Adding Question6

---
+++ week4/Combination.ps1
@@ -0,0 +1,67 @@
+﻿# Login and Logoff information function
+
+# Get login and logoff records from Widndows Events and save to a variable
+$loginouts = Get-EventLog system -Source Microsoft-Windows-winlogon -After (Get-Date).AddDays(-$chosendays)
+
+$loginoutsTable = @() #Empty Array to fill customly
+
+for($i=0; $i -lt $loginouts.Count; $i++){
+
+# creating event property value 
+$event = ""
+if($loginouts[$i].InstanceId -eq "7001") {$event="Logon"}
+if($loginouts[$i].InstanceId -eq "7002") {$event="Logoff"}
+
+# Creating user Property valuTe 
+# $user = $loginouts[$i].ReplacementStrings[1]
+$user = New-Object System.Security.Principal.SecurityIdentifier($loginouts[$i].ReplacementStrings[1])
+$account = $user.Translate([System.Security.Principal.NTAccount])
+$username = $account.Value
+
+
+# Adding each new line (in form of a custom object) to our empty array
+$loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated; `
+                                       "Id" = $loginouts[$i].InstanceId; `
+                                    "Event" = $event; `
+                                     "User" = $username; 
+                                     }
+} #End of for loop
+
+Write-Host "Giving the login and logoff events for the specified days"
+
+return $loginoutsTable
+
+# Computer start and stop 
+
+
+##################################################
+
+# Get computer start and stop records from Windows Events, then saving them to a variable
+# Have to use Where-Object to track system events 
+$startstop = Get-EventLog system -After (Get-Date).AddDays(-$chosendays) | `
+Where-Object {$_.EventID -eq 6005 -or $_.EventID -eq 6006}
+
+$StartStopTable = @() #Empty Array to fill customly
+
+for($i=0; $i -lt $startstop.Count; $i++){
+
+# creating event property value 
+$event = ""
+if($startstop[$i].EventID -eq "6005") {$event="Start"}
+if($startstop[$i].EventID -eq "6006") {$event="Shutdown"}
+
+# Creating a new user property value using System
+$username = "System"
+
+
+# Adding each new line (in form of a custom object) to our empty array
+$StartStopTable += [pscustomobject]@{"Time" = $startstop[$i].TimeGenerated; `
+                                       "Id" = $startstop[$i].EventID; `
+                                    "Event" = $event; `
+                                     "User" = $username; 
+                                     }
+} #End of for loop
+
+Write-Host "Giving the computer start and shutdown times for the specified days."
+
+return $StartStopTable
--- week4/Question6.ps1
+++ week4/Question6.ps1
@@ -1,12 +1,12 @@
-﻿.(Join-Path $PSScriptRoot Question4.ps1)
-.(Join-Path $PSScriptRoot Question5.ps1)
+﻿.(Join-Path $PSScriptRoot Combination.ps1)
 
 clear
 
 # Get Login and Logoffs from the last 15 days.
-$loginoutsTable = 
+$loginoutsTable = $chosendays = 15
 $loginoutsTable
 
 # Get Shutdowns and Start ups from the last 25 days.
-$StartStopTable = 
+$StartStopTable = $chosendays = 25
 $StartStopTable
+
