9f1ad731f9a09f08b2fe14a8a9ddd618befa9be0
Lily <LPouliot@champlain.edu>
2025-09-19 16:09:32 -0400

    Adding Question6

---
+++ week4/Question6.ps1
@@ -0,0 +1,12 @@
+﻿.(Join-Path $PSScriptRoot Question4.ps1)
+.(Join-Path $PSScriptRoot Question5.ps1)
+
+clear
+
+# Get Login and Logoffs from the last 15 days.
+$loginoutsTable = 
+$loginoutsTable
+
+# Get Shutdowns and Start ups from the last 25 days.
+$StartStopTable = 
+$StartStopTable
