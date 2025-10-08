26a805be93d9f77f1ef4ed51ba22a0c69bc01be2
Lily <LPouliot@champlain.edu>
2025-10-07 23:50:04 -0400

    updating!

--- week6/TurnToMenu.ps1
+++ week6/TurnToMenu.ps1
@@ -1,7 +1,7 @@
 ﻿. (Join-Path $PSScriptRoot ParsingApacheLogs.ps1)
-. (Join-Path $PSScriptRoot )
-. (Join-Path $PSScriptRoot )
-. (Join-Path $PSScriptRoot )
+. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot main.ps1)
+. (Join-Path $PSScriptRoot Question4.ps1)
 
 clear
 
@@ -18,3 +18,62 @@ $Prompt += "5 - Exit`n"
 $operation = $true
 
 while($operation){
+
+Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+
+    # Display last 10 apache logs 
+    # (Utilize function ApacheLogs1 from assignment Parsing Apache Logs)
+    if($choice -eq 1){
+        $tableRecords = (Get-Date).AddDays(-10) 
+        $tableRecords | Format-Table -AutoSize -Wrap 
+    }
+
+
+
+   
+    # Display last 10 failed logins for all users 
+    # (Utilize function getFailedLogins from lab Local User Management Menu)
+    if($choice -eq 2){
+
+    }
+
+
+
+    # Display at risk users 
+    # (Utilize your function from lab Local User Management Menu)
+    if($choice -eq 3){
+
+    }
+
+
+
+
+
+
+
+    # Start Chrome web browser and navigate it to champlain.edu
+    # if no instances of Chrome is running (from lab Process Management - 1)
+    if($choice -eq 4){
+
+    }
+
+
+
+
+
+
+
+    # Exit
+    elseif($choice -eq 5){
+        Write-Host "Goodbye" | Out-String
+        exit
+       }
+
+    # If user enters anything other than listed choices, e.g. a number that is not in the menu   
+    #       or a character that should not be accepted. Give a proper message to the user and prompt again.
+    else{
+        Write-Host "INVALID CHOICE. Enter a number between 1 and 5."
+    }
+}
