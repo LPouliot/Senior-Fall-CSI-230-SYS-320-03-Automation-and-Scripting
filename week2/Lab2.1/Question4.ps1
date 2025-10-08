4d5129275b1ca6bfcf3007ff1030bfa02a2b617f
Lily <LPouliot@champlain.edu>
2025-10-08 00:33:42 -0400

    updating!

--- week2/Lab2.1/Question4.ps1
+++ week2/Lab2.1/Question4.ps1
@@ -1,13 +1,15 @@
 ﻿# If an instance of it is not running already, starts Google Chrome web browser
 # If an instance is already running, stop it.
 
-
-if (Get-Process chrome -ErrorAction SilentlyContinue) {
-    Write-Host "Chrome is currently on, shutting down..."
-    Stop-Process -Name chrome 
-} else {
-    Write-Host "Chrome is currently off, turning on.."
-    Write-Host "Moving to Champlain.edu"
-    Start-Process chrome "https://champlain.edu"
+function StartStopChrome(){
+    if (Get-Process chrome -ErrorAction SilentlyContinue) {
+        Write-Host "Chrome is currently on, shutting down..."
+        Stop-Process -Name chrome 
+    } else {
+        Write-Host "Chrome is currently off, turning on.."
+        Write-Host "Moving to Champlain.edu"
+        Start-Process chrome "https://champlain.edu"
+    }
 }
 
+StartStopChrome
--- week6/TurnToMenu.ps1
+++ week6/TurnToMenu.ps1
@@ -1,42 +1,39 @@
-﻿. (Join-Path $PSScriptRoot ParsingApacheLogs.ps1)
-. (Join-Path $PSScriptRoot Users.ps1)
-. (Join-Path $PSScriptRoot main.ps1)
+﻿. (Join-Path $PSScriptRoot Users.ps1)
 . (Join-Path $PSScriptRoot Question4.ps1)
+. (Join-Path $PSScriptRoot ParsingApacheLogs.ps1)
 
 clear
 
-$Prompt = "`n"
-$Prompt += "Please choose your operation:`n"
-$Prompt += "1 - Display last 10 Apache Logs`n"
-$Prompt += "2 - Display last 1o failed logins`n"
-$Prompt += "3 - Display at risk users`n"
-$Prompt += "4 - Start Chrome Web Browser`n"
-$Prompt += "5 - Exit`n"
+$Message = "`n"
+$Message += "Please choose your operation:`n"
+$Message += "1 - Display last 10 Apache Logs`n"
+$Message += "2 - Display last 1o failed logins`n"
+$Message += "3 - Display at risk users`n"
+$Message += "4 - Start Chrome Web Browser`n"
+$Message += "5 - Exit`n"
 
 
 
-$operation = $true
+$Operations = $true
 
-while($operation){
+while($Operations){
 
-Write-Host $Prompt | Out-String
+Write-Host $Message | Out-String
     $choice = Read-Host 
 
 
     # Display last 10 apache logs 
     # (Utilize function ApacheLogs1 from assignment Parsing Apache Logs)
     if($choice -eq 1){
-        $tableRecords = (Get-Date).AddDays(-10) 
-        $tableRecords | Format-Table -AutoSize -Wrap 
+        ApacheLogs1 | Select-Object -Last 10 |Format-Table -AutoSize -Wrap 
     }
 
-
-
+   
    
     # Display last 10 failed logins for all users 
     # (Utilize function getFailedLogins from lab Local User Management Menu)
     if($choice -eq 2){
-
+        getFailedLogins 10 | Select-Object -Last 10 |Format-Table -AutoSize -Wrap 
     }
 
 
@@ -44,24 +41,24 @@ Write-Host $Prompt | Out-String
     # Display at risk users 
     # (Utilize your function from lab Local User Management Menu)
     if($choice -eq 3){
+   
+    $failedLogins = getFailedLogins 90
 
+        if($failedLogins){
+            $userFailedCounts = $failedLogins | Group-Object -Property User |`
+                            Select-Object Name, Count |`
+                            Sort-Object Count -Descending
+       
+            Write-Host ($userFailedCounts | Format-Table | Out-String)
     }
-
-
-
-
-
-
+}
+ 
 
     # Start Chrome web browser and navigate it to champlain.edu
     # if no instances of Chrome is running (from lab Process Management - 1)
     if($choice -eq 4){
-
+    StartStopChrome 
     }
-
-
-
-
 
 
 
@@ -71,9 +68,4 @@ Write-Host $Prompt | Out-String
         exit
        }
 
-    # If user enters anything other than listed choices, e.g. a number that is not in the menu   
-    #       or a character that should not be accepted. Give a proper message to the user and prompt again.
-    else{
-        Write-Host "INVALID CHOICE. Enter a number between 1 and 5."
-    }
 }
