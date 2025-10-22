50c694ea9093c117c2306f180cd8ad9020b6efef
Lily Pouliot <113251856+LPouliot@users.noreply.github.com>
2025-10-22 10:47:04 -0400

    Add files via upload

---
+++ week8/main.ps1
@@ -0,0 +1,17 @@
+﻿. "C:\Users\champuser\Senior-Fall-CSI-230-SYS-320-03-Automation-and-Scripting\week7\Email.ps1"
+. "C:\Users\champuser\Senior-Fall-CSI-230-SYS-320-03-Automation-and-Scripting\week7\Scheduler.ps1"
+. "C:\Users\champuser\Senior-Fall-CSI-230-SYS-320-03-Automation-and-Scripting\week6\Event-Logs.ps1"
+
+# Obtaining config
+$configuration = Get-Content "C:\Users\champuser\Senior-Fall-CSI-230-SYS-320-03-Automation-and-Scripting\week7\Configuration.txt"
+
+# Obtaining at risk users
+$Failed = getFailedLogins 10
+
+# Sending at risk users as email
+SendAlertEmail ($Failed | Format-Table | Out-String)
+
+# Setting the script to be run daily
+ChooseTimeToRun($configuration[1])
+
+
