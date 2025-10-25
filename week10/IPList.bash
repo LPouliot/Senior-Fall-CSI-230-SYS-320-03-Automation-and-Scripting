863d0b770bd631f2bfb61da5300d1b7c50b2009a
LPouliot <lily.pouliot@mymail.champlain.edu>
2025-10-24 20:16:04 -0400

    updating

--- week10/IPList.bash
+++ week10/IPList.bash
@@ -9,6 +9,12 @@
 # Prefix is the first input taken
  prefix="$1"
 
+# Verify input length
+[ the_input -lt 5] && \
+printf "Prefix legnth is too short\nPrefix example: 10.0.17\n" && \
+exit 1
+
+
 for i in $(seq 1 254)
 do
         echo "$prefix.$i"
