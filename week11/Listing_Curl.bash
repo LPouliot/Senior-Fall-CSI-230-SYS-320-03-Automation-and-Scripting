0d275a4c86c83538204feb76815a9b2874515f92
lily <lily.pouliot@mymail.champlain.edu>
2025-11-06 23:44:04 -0500

    updating

--- week11/Listing_Curl.bash
+++ week11/Listing_Curl.bash
@@ -1,12 +1,11 @@
 #!bin/bash
 
-allLogs=""
 file="/var/log/apache2/access.log"
 
 # Creating a function to count every curl access 
 
 function countingCurlAccess(){
-  curlAccess=$(cat "$allLogs" | grep "curl" | cut -d' ' -f1 | sort | uniq -c)
+  curlAccess=$(cat "$file" | grep "curl" | cut -d' ' -f1,12 )
 }
 
 countingCurlAccess
