8f1734d8aa03806a2c5687898802e093a06c5eb9
lily <lily.pouliot@mymail.champlain.edu>
2025-11-15 16:15:28 -0500

    updating

--- week12/access.bash
+++ week12/access.bash
@@ -1,15 +1,15 @@
-
 #!/bin/bash
 
 # Narrowing to get date and time
-dateTime=$(date "+%a %b %d %I:%M:%S %p %Z %Y")
+FILE="fileaccesslog.txt"
+ACCESSED="$1"
+TIMESTAMP=$(date "+%Y-%m-%d %H-%M-%S")
 
-# Logging to fileaccesslog.txt
-echo "File was Accessed at $datetime" > /home/champuser/Senior-Fall-CSI-230-SYS-320-03-Automation-and-Scripting/week12/fileaccesslog.txt
+# Log the file
+echo "[$TIMESTAMP] File Accessed: $ACCESSED" >> "$FILE"
 
-# Sending Email
-echo "To: lily.pouliot@mymail.champlain.edu" >> fileaccesslog.txt
+# Send email 
+echo "To: lily.pouliot@mymail.champlain.edu" > fileaccesslog.txt
 echo "Subject: Access" >> fileaccesslog.txt
-
-cat  fileaccesslog.txt | ssmtp lily.pouliot@mymail.champlin.edu
-
+cat "$FILE" >> fileaccesslog.txt
+cat fileaccesslog.txt | ssmtp lily.pouliot@mymail.champlain.edu 
