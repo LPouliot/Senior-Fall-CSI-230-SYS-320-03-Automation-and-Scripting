fd7daa1f4495540bc5f3936893d0a419b9070578
lily <lily.pouliot@mymail.champlain.edu>
2025-12-01 14:25:16 -0500

    adding new files

--- week13/ReadingTables.bash
+++ week13/ReadingTables.bash
@@ -2,6 +2,7 @@
 
 # Grabbing  table data using xmlstarlet
 echo ""
+# position()>1 helps with  getting all of the rows except the first one, since the first one is the HTML header
 xmlstarlet sel -H -t -m "//table/tr[position()>1]" \
  -v "td[1]" -o "| " \
  -v "td[2]" -o " |" \
