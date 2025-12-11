d11263de9d3eeb85f5258ccc045d9f1611ec0438
lily <lily.pouliot@mymail.champlain.edu>
2025-12-10 21:58:01 -0500

    update

--- week14/finalC2.bash
+++ week14/finalC2.bash
@@ -1,14 +1,20 @@
 #!/bin/bash
-
-echo "Enter the file being scanned:" 
+echo "Enter the file being scanned:"
 read logfile
-
 echo "Enter the scanning file:"
 read iocfile
 
 # Searching for all of the indicators within IOC.txt
 while IFS= read -r ioc;
 do
-	[[ -z "$ioc" ]] &&  continue
-	grep -F "$ioc" "$logfile" | awk '{print $1, $2, $3'} > report.txt
-done < "iocfile"
+	# Strip leading/trailing whitespace
+	ioc=$(echo "$ioc" | xargs)
+
+	[[ -z "$ioc" ]] && continue
+
+	grep -F "$ioc" "$logfile" | awk '{print $1, $4, $7}' > report.txt
+
+done < "$iocfile"
+
+echo "glancing at report.txt file:"
+cat report.txt
